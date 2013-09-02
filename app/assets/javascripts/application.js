c0febc5456c0a04f8dae72523484d6d1e6cb2976
carlqt <carlwilliam.tablante@gmail.com>
2013-09-02 09:55:41 +0800

    standard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,13 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// Loads all Bootstrap javascripts
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
+
+$('.carousel').carousel({
+  interval: 2000
+});
